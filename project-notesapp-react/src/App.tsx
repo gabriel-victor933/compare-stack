2551f4d01f442d3e9c42a300ee22aa2bdaff284d
gabriel-victor933 <gabrielvictor933@usp.br>
2023-11-09 11:16:50 -0300

    adding edit function to single notes

--- project-notesapp-react/src/App.tsx
+++ project-notesapp-react/src/App.tsx
@@ -5,14 +5,15 @@ import Notes from './components/Notes'
 import { Note } from './components/models'
 
 type Action = {
-  type: "add" | "del",
+  type: "add" | "del" | "edit",
   payload: Note
 }
 
 function reducer(state: Note[],action: Action){
   switch(action.type){
     case "add": return [...state,action.payload];
-    case "del": return state.filter((note) => note.id != action.payload.id)
+    case "del": return state.filter((note) => note.id != action.payload.id);
+    case "edit": return state.map((note)=> note.id != action.payload.id ? note : action.payload)
     default: return state;
   }
 }
--- project-notesapp-react/src/components/SingleNote.tsx
+++ project-notesapp-react/src/components/SingleNote.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Note } from './models';
+import { AiOutlineClose, AiOutlineEdit } from "react-icons/ai";
 
 interface Props {
     note: Note
     dispatch: React.Dispatch<{
-      type: "add" | "del",
+      type: "add" | "del" | "edit",
       payload:Note;
   }>
 }
 
 const SingleNote:React.FC<Props> = ({note,dispatch}) => {
-  return (
+
+  const [openEdit,setOpenEdit] = useState<boolean>(false)
+  const [singleNote, setSingleNote] = useState<Note>(note)
+
+  function handleInputChange(e:  React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>){
+      setSingleNote({...singleNote,[e.target.name]:e.target.value})
+  }
+
+  function handleEditInputSubmit(e: React.FormEvent){
+      e.preventDefault()
+      dispatch({type: "edit",payload: singleNote})
+      setOpenEdit(false)
+  }
+
+  return (!openEdit ? 
     <div className='note'>
         <h1>{note.title}</h1>
         <p>{note.content}</p>
-        <small onClick={()=>dispatch({type: "del",payload: note})}>X</small>
+        <AiOutlineClose className="icon close" onClick={()=>dispatch({type: "del",payload: note})} />
+        <AiOutlineEdit className="icon edit" onClick={()=>setOpenEdit(true)}/>
+    </div> :
+    <div className='note' >
+      <form onSubmit={handleEditInputSubmit}>
+        <input className='note-input' name="title" value={singleNote.title} onChange={handleInputChange}/>
+        <textarea  className='note-textarea' name="content" value={singleNote.content} onChange={handleInputChange}/>
+        <button className='note-button'>Edit Note</button>
+      </form>
+      <AiOutlineClose className="icon close" onClick={()=>setOpenEdit(false)} />
     </div>
   )
 }
